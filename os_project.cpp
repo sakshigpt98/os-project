4964bc0d0df1cd3c6bfcd771a0c561968d512fac
sakshigpt98 <37805225+sakshigpt98@users.noreply.github.com>
2018-04-10 23:33:43 +0530

    Add files via upload

---
+++ os_project.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<pthread.h>
+int readcount=0;
+int shared=1;
+pthread_mutex_t wrt,red;
+void *reader(void *arv)
+{
+	printf("reader is trying to enter....\n");
+	pthread_mutex_lock(&red);
+   	readcount++;
+	if(readcount==1)
+	 pthread_mutex_lock(&wrt);
+	printf("reader inside the reader function =%d\n",readcount);
+	printf("value of shared in reader=%d",shared);
+	pthread_mutex_unlock(&red);
+	//sleep(2);
+	pthread_mutex_lock(&red);
+	readcount--;
+	if(readcount=0)
+	 pthread_mutex_unlock(&wrt);
+	printf("reader trying to leaving...\n");
+	pthread_mutex_unlock(&red);
+
+	
+}
+void *writer(void *a)
+{
+	printf("writer are trying to enter...\n");
+	pthread_mutex_lock(&wrt);
+	printf("writer entered...\n");
+	shared++;
+	printf("value of shared in writer=%d",shared);
+	//sleep(2);
+	printf("reader inside writerfunctionn=%d\n",readcount);
+	printf("writer leaving...\n");
+	pthread_mutex_unlock(&wrt);
+	//sleep(2);	
+}
+int main()
+{
+	    void* result;
+		pthread_t t1[50],t2[50];
+		int n1,n2,i;
+		printf("enter reader=");
+		scanf("%d",&n1);
+	    printf("enter  writer");
+		scanf("%d",&n2);
+		for(i=0;i<n1;i++)
+		pthread_create(&t1[i],NULL,reader,NULL);
+		for(i=0;i<n2;i++)
+		pthread_create(&t1[i],NULL,writer,NULL);
+		for(i=0;i<n2;i++)
+		pthread_join(t1[i],&result);
+		for(i=0;i<n1;i++)
+		pthread_join(t2[i],&result);
+		return 0;
+
+}
